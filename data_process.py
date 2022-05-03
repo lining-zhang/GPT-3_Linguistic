2558aa297cc8b390cdfb42757ec4d557b1e12767
Lining Zhang <lining_zhang@linings-mbp.mynetworksettings.com>
2022-05-02 23:21:17 -0400

    add data

---
+++ data_process.py
@@ -0,0 +1,33 @@
+import random
+random.seed(12)
+
+def data_fetch(path):
+    with open(path) as f:
+        result_list = []
+        for line in f:
+            label, sentence = line.split("\t")[1], line.split("\t")[2]
+            result_list.append(sentence + "\t" + label)
+
+    pair_list = random.sample(result_list, 1000)
+    return pair_list
+
+def write_txt(filename, content_list):
+    with open(filename + ".txt", "w") as f:
+        for item in content_list:
+            f.write("%s\n" % item)
+
+def main():
+    tense_path = "/Users/lining_zhang/Desktop/MSDS/1012--NLU/past_present.txt"
+    obj_path = "/Users/lining_zhang/Desktop/MSDS/1012--NLU/obj_number.txt"
+    subj_path = "/Users/lining_zhang/Desktop/MSDS/1012--NLU/subj_number.txt"
+
+    tense_pair_list = data_fetch(tense_path)
+    obj_pair_list = data_fetch(obj_path)
+    subj_pair_list = data_fetch(subj_path)
+
+    write_txt("tense_data", tense_pair_list)
+    write_txt("obj_num_data", obj_pair_list)
+    write_txt("subj_num_data", subj_pair_list)
+
+if __name__ == '__main__':
+    main()
